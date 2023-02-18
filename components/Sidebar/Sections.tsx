515f11758e349af80986c33ee06610ef09528e26
lilnako <nlunardi@hotmail.com>
2023-02-18 14:51:05 +1100

    feat: added section switching from sidebar

--- components/Sidebar/Sections.tsx
+++ components/Sidebar/Sections.tsx
@@ -4,13 +4,22 @@ interface Props {
   icon: React.ReactNode;
   text: string;
   href: string;
+  isOpen: boolean;
+  isActive: boolean;
 }
 
-function Sections({ icon, text, href }: Props) {
+function Sections({ icon, text, href, isOpen, isActive }: Props) {
   return (
-    <Link className="flex flex-row space-x-2 mb-4" href={href}>
-      <div className="h-5 w-5">{icon}</div>
-      <div>{text}</div>
+    <Link
+      className={`flex flex-row space-x-2 mb-6 ${
+        isActive ? "text-greenAccent-500" : "text-inherit"
+      }`}
+      href={href}
+    >
+      <div className="h-5 w-5 hover:scale-110 transition duration-200 hover:text-greenAccent-400">
+        {icon}
+      </div>
+      {isOpen && <div>{text}</div>}
     </Link>
   );
 }
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,6 +1,8 @@
-// "use client";
+"use client";
 
+import { useState } from "react";
 import Sections from "./Sections";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { HomeIcon } from "@heroicons/react/24/outline";
 import { ClipboardDocumentListIcon } from "@heroicons/react/24/outline";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
@@ -8,42 +10,82 @@ import { UserGroupIcon } from "@heroicons/react/24/outline";
 import { NewspaperIcon } from "@heroicons/react/24/outline";
 import { CalendarDaysIcon } from "@heroicons/react/24/outline";
 
-export default function Sidebar() {
-  return (
-    <div className="p-4 py-10 w-[16rem] bg-primary-400 text-primary-100">
-      <h1 className="text-3xl">The Watchlist</h1>
+interface SectionInfo {
+  icon: React.ReactNode;
+  text: string;
+  href: string;
+  name: string;
+}
 
-      <div className="my-20 mx-4">
-        <Sections
-          icon={<HomeIcon></HomeIcon>}
-          text="Dashboard"
-          href="/"
-        ></Sections>
-        <Sections
-          icon={<ClipboardDocumentListIcon></ClipboardDocumentListIcon>}
-          text="My List"
-          href="/my-list"
-        ></Sections>
-        <Sections
-          icon={<UserCircleIcon></UserCircleIcon>}
-          text="Players"
-          href="players"
-        ></Sections>
-        <Sections
-          icon={<UserGroupIcon></UserGroupIcon>}
-          text="Teams"
-          href="teams"
-        ></Sections>
-        <Sections
-          icon={<NewspaperIcon></NewspaperIcon>}
-          text="News"
-          href="news"
-        ></Sections>
-        <Sections
-          icon={<CalendarDaysIcon></CalendarDaysIcon>}
-          text="Fixtures"
-          href="/fixtures"
-        ></Sections>
+const sections: SectionInfo[] = [
+  {
+    icon: <HomeIcon />,
+    text: "Dashboard",
+    href: "/",
+    name: "dashboard",
+  },
+  {
+    icon: <ClipboardDocumentListIcon />,
+    text: "My List",
+    href: "/my-list",
+    name: "my-list",
+  },
+  {
+    icon: <UserCircleIcon />,
+    text: "Players",
+    href: "/players",
+    name: "players",
+  },
+  {
+    icon: <UserGroupIcon />,
+    text: "Teams",
+    href: "/teams",
+    name: "teams",
+  },
+  {
+    icon: <NewspaperIcon />,
+    text: "News",
+    href: "/news",
+    name: "news",
+  },
+  {
+    icon: <CalendarDaysIcon />,
+    text: "Fixtures",
+    href: "/fixtures",
+    name: "fixtures",
+  },
+];
+
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isActive, setIsActive] = useState<string>("dashboard");
+
+  return (
+    <div
+      className={`p-4 py-3 ${
+        isOpen ? "w-[16rem]" : "w-[5rem]"
+      } bg-primary-400 text-primary-100`}
+    >
+      <div
+        className="flex justify-end hover:text-primary-200 transition ease-in-out duration-200 cursor-pointer"
+        onClick={(e) => setIsOpen((curr) => !curr)}
+      >
+        <Bars3Icon className="h-8 w-8" />
+      </div>
+      <h1 className="text-3xl py-6">{isOpen ? "The Watchlist" : "WL"}</h1>
+
+      <div className={`my-20 ${isOpen ? "mx-4" : "mx-2"}`}>
+        {sections.map((section, idx) => (
+          <div key={idx} onClick={(e) => setIsActive(section.name)}>
+            <Sections
+              icon={section.icon}
+              text={section.text}
+              href={section.href}
+              isOpen={isOpen}
+              isActive={isActive === section.name ? true : false}
+            ></Sections>
+          </div>
+        ))}
       </div>
     </div>
   );
